a4425037a37c1a136e971958b3faae4137fbf146
kerren <kerren@entrostat.com>
2018-09-22 14:08:42 +0200

    Wasn't building the docker image correctly

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ echo "export SERVER_NAME=$name" >> /etc/profile
 
 curl -sSL https://get.docker.com/ | sh
 
-docker build -t hello/server
+docker build ./ -t hello/server
 
 docker run -p 3000:3000 -e PORT=$PORT -e SERVER_NAME=$SERVER_NAME hello/server -d
