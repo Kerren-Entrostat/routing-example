61307ba3f916461ab13ac8ea7d09cf5c2fd2a6ee
kerren <kerren@entrostat.com>
2018-09-22 13:58:54 +0200

    Added classic to the snap installer

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ read name
 echo "export PORT=3000" >> /etc/profile
 echo "export SERVER_NAME=$name" >> /etc/profile
 
-sudo snap install --edge node
+sudo snap install --edge node --classic
 npm install
 npm install -g pm2@latest
 
