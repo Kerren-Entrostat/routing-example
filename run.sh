e151170c8dddaa07c2670cbfb7f612c80ec82b8a
kerren <kerren@entrostat.com>
2018-09-22 17:29:53 +0200

    Removed the profile exports

--- run.sh
+++ run.sh
@@ -2,11 +2,9 @@
 echo "Enter the name of the server:"
 read name
 
-echo "export PORT=3000" >> /etc/profile
-echo "export SERVER_NAME=$name" >> /etc/profile
 
 curl -sSL https://get.docker.com/ | sh
 
 docker build ./ -t hello/server
 
-docker run -p 3000:3000 -e PORT=$PORT -e SERVER_NAME=$SERVER_NAME -d hello/server:latest
+docker run -p 3000:3000 -e SERVER_NAME=$name -d hello/server:latest
